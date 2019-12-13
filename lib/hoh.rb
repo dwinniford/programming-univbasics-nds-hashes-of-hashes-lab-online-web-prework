95d7d3605093526afe7a39931d959f7e2739e699
darling-flag-8189 <dwinniford@gmail.com>
2019-12-13 16:49:20 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -25,14 +25,15 @@ PORTION_3 = {
   }
  }
  
-# PORTION_4 = {
-#   label: "Species",
-#   sub_category: nil
-# }
+PORTION_4 = {
+  label: "Species",
+ sub_category: nil
+ }
 
 def naming_system
   PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
   PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+  PORTION_1[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_4
   PORTION_1
   # Remember:
   #  Kingdom
